6ec36546fb7357b728d67b7080ea31accb58cee7
Sajid Hussain <80850448+Sajid-tech@users.noreply.github.com>
2023-06-10 16:09:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const replaceThis = "sajid"
+const replaceWith = "majid"
+
+const preview  = true
+
+
+try{
+     fs.readdir("data",(err,data)=>{
+
+        console.log(data);
+        for (let index = 0; index < data.length; index++) {
+            const item = data[index];
+            let newFile = "data/" + item.replaceAll(replaceThis,replaceWith)
+            if(!preview){
+        
+        fs.rename("data/"+ item , newFile,()=>{
+            console.log("Rename Sucess")
+        })
+    }
+    else{
+        if("data/"+ item !==newFile)
+        console.log("data/"+ item+ "will be renamed to " + newFile)
+    }
+    }
+    })
+} catch(err){
+    console.error(err); 
+}
